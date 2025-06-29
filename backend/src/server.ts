768c54b530cc677e9cf02de6cdf20ea8606ca93d
Amritha <169130861+Amruthapallavi@users.noreply.github.com>
2025-06-29 22:11:43 +0530

    cors updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,28 +11,14 @@ const app= express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = ['https://short-url-fawn-nu.vercel.app',
-'https://short-url-amrithas-projects-57839655.vercel.app',
-'https://short-2eusxd586-amrithas-projects-57839655.vercel.app'
-];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); 
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('Not allowed by CORS'), false);
-    }
-    return callback(null, origin);
-  },
-  credentials: true,
-}));
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 
 app.use("/user", userRoutes);
